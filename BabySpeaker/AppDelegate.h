5bf67bea0bf752a89a31df57684ba61d852ad665
DavidKakaladze <Davjob15@gmail.com>
2016-01-02 13:26:15 +0300

    Initial Commit

---
+++ BabySpeaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabySpeaker
+//
+//  Created by David Kakaladze on 02.01.16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabySpeaker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BabySpeaker
+//
+//  Created by David Kakaladze on 02.01.16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
